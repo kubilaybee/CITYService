cfa816bd77ea425e8fff80723e2f736a42fa4997
kubilaybee <kubilay.bekdemir@anymile.io>
2023-05-01 14:17:18 +0300

    REST step-1 completed

--- src/main/java/qb/lie/cityservice/controller/CityController.java
+++ src/main/java/qb/lie/cityservice/controller/CityController.java
@@ -1,29 +1,75 @@
 package qb.lie.cityservice.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import qb.lie.cityservice.model.City;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/cities")
 public class CityController {
 
+    private static final List<City> cities = new ArrayList<>();
+
+    public CityController(){
+        if (cities.isEmpty()){
+            City city1 = new City(new Date(),"28","Giresun");
+            City city2 = new City(new Date(),"34","Istanbul");
+            City city3 = new City(new Date(),"06","Ankara");
+
+            cities.add(city1);
+            cities.add(city2);
+            cities.add(city3);
+        }
+    }
+
     @GetMapping
     public ResponseEntity<List<City>> getCities() {
 
-        City city1 = new City("28","Giresun");
-        City city2 = new City("34","Istanbul");
-        City city3 = new City("06","Ankara");
-
-        List<City> cities = Arrays.asList(city1,city2,city3);
-
         return new ResponseEntity<>(cities, OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<City> getCity(@PathVariable String id){
+        City result = getCityById(id);
+
+        return new ResponseEntity<>(result,OK);
+    }
+
+    private City getCityById(String id) {
+        return cities.stream()
+                .filter(city -> city.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("City not Found"));
+    }
+
+    @PostMapping
+    public ResponseEntity<City> createCity(@RequestBody City newCity){
+        newCity.setCreateDate(new Date());
+        cities.add(newCity);
+
+        return new ResponseEntity<>(newCity,CREATED);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> getCity(@PathVariable String id,@RequestBody City city){
+        getCityById(id).setName(city.getName());
+        getCityById(id).setCreateDate(new Date());
+
+        return new ResponseEntity<>(OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCity(@PathVariable String id){
+        cities.remove(getCityById(id));
+
+        return new ResponseEntity<>(OK);
     }
 }
--- src/main/java/qb/lie/cityservice/model/City.java
+++ src/main/java/qb/lie/cityservice/model/City.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class City {
-
+    private Date createDate;
     private String id;
     private String name;
 
